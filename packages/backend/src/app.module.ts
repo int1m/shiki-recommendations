1901b63cc3c87e9a7d8d0f0e85f7fe32e379f1d5
int1m <mostovoy@volsu.ru>
2022-09-24 22:14:21 +0300

    chore(backend): create connect to mariaDB in docker. #issueID shihi-33

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/configs/.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get<string>('MARIADB_HOST'),
+        port: configService.get<number>('MARIADB_PORT'),
+        username: configService.get<string>('MARIADB_ROOT_USER'),
+        password: configService.get<string>('MARIADB_ROOT_PASSWORD'),
+        database: configService.get<string>('MARIADB_DATABASE'),
+        synchronize: true,
+        logging: true,
+        entities: [],
+        subscribers: [],
+        migrations: [],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
