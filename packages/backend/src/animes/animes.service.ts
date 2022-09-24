925b082a5b0a269855eb51130bb2b5c3d600dce8
int1m <mostovoy@volsu.ru>
2022-09-24 22:43:45 +0300

    chore(backend): create test CRON task. #issueID shihi-36

--- packages/backend/src/animes/animes.service.ts
+++ packages/backend/src/animes/animes.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
 import { CreateAnimeDto } from './dto/create-anime.dto';
 import { UpdateAnimeDto } from './dto/update-anime.dto';
 
 @Injectable()
 export class AnimesService {
+  private readonly logger = new Logger(AnimesService.name);
+
   create(createAnimeDto: CreateAnimeDto) {
     return 'This action adds a new anime';
   }
@@ -22,5 +26,24 @@ export class AnimesService {
 
   remove(id: number) {
     return `This action removes a #${id} anime`;
+  }
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async onCronAnimesUpdateHandler() {
+    const headers = new Headers({
+      'User-Agent': 'shikimori-recommendations',
+    });
+    const params: Record<string, string> = {
+      limit: '50',
+      page: '1',
+    };
+
+    const response = await fetch(`https://shikimori.one/api/animes?${new URLSearchParams(params).toString()}`, {
+      method: 'GET',
+      headers,
+    });
+    const result = await response.json();
+
+    this.logger.log(`url: ${response.url}. result: ${JSON.stringify(result)}`);
   }
 }
--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AnimesModule } from './animes/animes.module';
 
 @Module({
@@ -9,6 +11,7 @@ import { AnimesModule } from './animes/animes.module';
       envFilePath: `${process.cwd()}/configs/.env.${process.env.NODE_ENV || 'development'}`,
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
