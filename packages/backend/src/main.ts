a161fcde55872fe5cf7c3928e0fcfd9688ab25ae
int1m <mostovoy@volsu.ru>
2022-10-04 17:47:18 +0300

    feat(backend): implement OpenAPI #issueID shiki-78

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
@@ -15,6 +16,15 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Animes documentation')
+    .setDescription('The animes API description')
+    .setVersion('1.0')
+    .addTag('animes')
+    .build();
+  const openApiInstance = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api', app, openApiInstance);
 
   await app.listen(3000);
 }
