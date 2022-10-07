3c81ff9036e0983cdc4e53d6fdb44f2b25f5ffa8
int1m <mostovoy@volsu.ru>
2022-10-07 20:08:40 +0300

    fix(backend): startup

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,5 +1,7 @@
+import * as fs from 'fs';
+
 import { NestFactory } from '@nestjs/core';
-import { VersioningType } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
@@ -7,9 +9,19 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const httpsOptions = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined ? {
+    key: fs.readFileSync(`${process.cwd()}/tools/mkcert/key.pem`),
+    cert: fs.readFileSync(`${process.cwd()}/tools/mkcert/cert.pem`),
+  } : undefined;
+
+  Logger.log(`Process ENV: ${process.env.NODE_ENV}`, 'ENV');
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      logger: true,
+      https: httpsOptions,
+    }),
   );
 
   app.enableVersioning({
@@ -17,17 +29,17 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
-  // const openApiConfig = new DocumentBuilder()
-  //   .setTitle('Animes documentation')
-  //   .setDescription('The animes API description')
-  //   .setVersion('1.0')
-  //   .addTag('animes')
-  //   .build();
-  // const openApiInstance = SwaggerModule.createDocument(app, openApiConfig);
-  // SwaggerModule.setup('api', app, openApiInstance);
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Animes documentation')
+    .setDescription('The animes API description')
+    .setVersion('1.0')
+    .addTag('animes')
+    .build();
+  const openApiInstance = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api', app, openApiInstance);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+// eslint-disable-next-line no-void
+void bootstrap();
