4d392737d686f62a47a9fba244490f1861c60690
int1m <mostovoy@volsu.ru>
2022-11-12 18:30:58 +0300

    feat(machine-learning): personal-recommendations route

--- packages/machine-learning/src/app.py
+++ packages/machine-learning/src/app.py
@@ -4,3 +4,11 @@ from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 
 app = FastAPI(title="volsu-rating-bot")
+
+@app.post("/personal-recommendations", response_class=PlainTextResponse)
+async def boot(request: Request) -> str:
+    data = await request.json()
+    print(data)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
