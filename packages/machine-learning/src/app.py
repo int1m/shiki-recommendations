7a248d6376681b189b1d511141f2d500ddcff492
pont1s <maxmonsta@gmail.com>
2022-11-20 15:19:03 +0300

    feat: docker-compose open db

--- packages/machine-learning/src/app.py
+++ packages/machine-learning/src/app.py
@@ -1,12 +1,31 @@
 import uvicorn as uvicorn
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from starlette.requests import Request
-from starlette.responses import PlainTextResponse
+from starlette.responses import PlainTextResponse, JSONResponse
+
 from RecommenderSystem.neuronet import Neuronet
 
 app = FastAPI(title="shikireki-machine-learning")
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 neuronet = Neuronet()
+
 
 @app.on_event("startup")
 async def startupEvent():
@@ -14,17 +33,31 @@ async def startupEvent():
         neuronet.trainModel()
     neuronet.uploadWeights()
 
-@app.post("/personal-recommendations", response_class=PlainTextResponse)
-async def onPersonalRecommendationsHandler(request: Request) -> str:
-    print('ok')
-    # data = await request.json()
-    # print(data)
-    # if (neuronet.isReady):
 
-@app.post("/similar-animes", response_class=PlainTextResponse)
+@app.post("/personal-recommendations", response_class=JSONResponse)
+async def onPersonalRecommendationsHandler(request: Request):
+    data = await request.json()
+    if (neuronet.isReady):
+        predictionVector = neuronet.predict(data)
+        animes = list()
+        for score in predictionVector[0:10]:
+            animeDf = neuronet.df_animes.loc[score['index']][['name', 'externalId']]
+            animes.append({
+                'name': animeDf['name'],
+                'externalId': animeDf.externalId.item(),
+            })
+        return animes
+
+    return {
+        'errors': ['Neuronet is not ready'],
+    }
+
+
+@app.get("/similar-animes", response_class=PlainTextResponse)
 async def onSimilarAnimes(request: Request) -> str:
     data = await request.json()
     print(data)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
